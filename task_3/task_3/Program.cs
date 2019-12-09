ce1f3ba8bc1280b848b11a19cb4bfd7c08712bbe
Vika Strigo <vika120519841330@gmail.com>
2019-12-10 00:56:35 +0300

    task_3

---
+++ task_3/task_3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Уменьшите размер функции.
+static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+{
+        bool shouldFire = true;
+        if (enemyInFront == true)
+        {
+            if (enemyName == "boss")
+            {
+                if (robotHealth < 50) shouldFire = false;
+                if (robotHealth > 100) shouldFire = true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return shouldFire;
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShouldFire(true, "other", 60));
+            Console.ReadKey();
+        }
+        static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            if ((enemyInFront == false) || ((enemyName == "boss") && (robotHealth < 50)))
+                return false;
+            else return true;
+        }
+    }
+}
